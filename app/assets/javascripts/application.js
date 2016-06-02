cafa835519d2580eeaf08053a3b637ddf8043abd
Pedro <pedro@bydlowski.com>
2016-06-02 12:03:12 -0700

    awesomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require awesomplete
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
