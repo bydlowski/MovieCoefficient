b331a86e11a0d5a2176894d637c2717b46bf2c48
Pedro <pedro@bydlowski.com>
2016-06-02 13:56:33 -0700

    Controller Calculations

--- app/controllers/coeficient_controller.rb
+++ app/controllers/coeficient_controller.rb
@@ -2,16 +2,24 @@ class CoeficientController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+
+    @user_imdb_rating = current_user.imdb_rating
     
   end
 
   def result
+
+    # UPDATE USER RATINGS
     @user_rating = params[:result_path][:user_rating]
+    #IMDB
     @film_imdb_rating = params[:result_path][:form_imdb_rating]
+    
+
     respond_to do |format|
       format.html
       format.js
-    end
+    end  
+    
   end
 
 end
