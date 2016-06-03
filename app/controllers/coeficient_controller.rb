7acd0313845e4081d222f0c1996b1d4fd9e45db1
Pedro <pedro@bydlowski.com>
2016-06-03 06:31:45 -0700

    Update database 4

--- app/controllers/coeficient_controller.rb
+++ app/controllers/coeficient_controller.rb
@@ -12,9 +12,16 @@ class CoeficientController < ApplicationController
 
     # UPDATE USER RATINGS
     @user = current_user
+    # Get ratings given by the user
     @user_rating = params[:result_path][:user_rating]
     @film_imdb_rating = params[:result_path][:form_imdb_rating]
-    #@user.update(imdb_rating: @film_imdb_rating)
+
+    # Compare ratings user/website
+    def percent_difference(u_rating, w_rating)
+      absolute_difference = (u_rating - w_rating).abs
+      result = 1 - (absolute_difference / 4)
+    end
+
     if @user.update(rating_param)
       @user.update(imdb_rating: @film_imdb_rating)
     else
