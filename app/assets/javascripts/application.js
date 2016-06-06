c7cb873bfcf41efb0adb21081f7606d5e6bd22d3
Pedro <pedro@bydlowski.com>
2016-06-06 08:37:16 -0700

    Before foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,17 @@
 //= require_tree .
 $(document).ready(function() {
 	$('#countries').val('');
-	$('#textSubmit').val('');
+	$('#textSubmit').val('5');
+	$("#the_form form").on('mousemove', function() {
+	  $form = $(this);
+	  $input_value = $form.children("#textSubmit").val();
+	  $label = $form.children("#textSubmit").siblings("label");
+	  $label.text("Current Value = " + $input_value);
+	})
+	$("#the_form form").on('keyup', function() {
+	  $form = $(this);
+	  $input_value = $form.children("#textSubmit").val();
+	  $label = $form.children("#textSubmit").siblings("label");
+	  $label.text("Current Value = " + $input_value);
+	})
 });
